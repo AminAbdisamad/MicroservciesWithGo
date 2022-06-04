3c7334c32c90502d86cec98a94ec67c7f1a66d4f
Amin <theaminux@gmail.com>
2022-06-04 22:40:31 +0300

    :dart: cmt

--- main.go
+++ main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-const PORT = "2022"
+const PORT = ":2022"
 
+func home(writer http.ResponseWriter, request *http.Request) {
+	log.Println("API Running")
+	data, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		http.Error(writer, "Something went wrong", http.StatusBadRequest)
+		log.Println(err)
+		return
+	}
+	fmt.Fprintf(writer, "Hello %s", data)
+
+}
 func main() {
-	http.HandleFunc(
-		"/", func(writer http.ResponseWriter, request *http.Request) {
-			log.Println("API Running")
-			data, err := ioutil.ReadAll(request.Body)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			log.Printf("Data Received: %s\n\n", data)
-
-		},
-	)
-	err := http.ListenAndServe(":2022", nil)
+	http.HandleFunc("/", home)
+	err := http.ListenAndServe(PORT, nil)
 	if err != nil {
 		log.Println(err)
 		return
